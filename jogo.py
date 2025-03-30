adecb977a0fe620ed17830bc44b2afa139c8c09f
Enzo Olivato <enzopazian1402@gmail.com>
2025-03-30 15:05:45 -0300

    Aprimoramento do looping de funcionamento do jogo e adição do looping do palpite

--- jogo.py
+++ jogo.py
@@ -1,6 +1,7 @@
-jogar = 1
+jogar = cont = 1
 numGerado = aux = 1234
 numPalpite = 0
+dica1 = dica2 = dica3 = dica4 = 0
 
 # Desmembramento do número gerado
 milhar = aux//1000
@@ -18,9 +19,35 @@ while jogar == 1:
     print('A partir da 5ª tentativa, o jogo irá te ajudar com dicas.\n\tBoa sorte!!')
     avanco = input('\n\t<< Tecle Enter para continuar >>')
 
+    print('-'*60)
+    # Palpite inicial
+    aux = numPalpite = int(input("\n\t>> 1ª Tentativa <<\n\nDigite seu palpite: "))
+
     # Looping da advinhação
-    
+    while cont <= 10:
+
+        # Verificação de vitória
+        if numPalpite != numGerado:
+            print('Perdeu')
+            # Desmembramento do número recebido
+            milhar2 = aux//1000
+            aux -= milhar2*1000
+            centena2 = aux//100
+            aux -= centena2*100
+            dezena2 = aux//10
+            aux -= dezena2*10
+            unidade = aux
+
+            # Adicionar o início das validações: dicas e exibição do código
+
+            cont += 1
+            aux = numPalpite = int(input(f"\n\t>> {cont}ª Tentativa <<\nDigite seu palpite: "))
+        else:
+            print('Venceu')
+            break
     
     jogar = int(input('\nDeseja jogar novamente?\n[1] - SIM\n[0] - NÂO\n\n>> '))
+    while jogar != 1 and jogar !=0:
+        jogar = int(input('\nOpção inválida\nDeseja jogar novamente?\n[1] - SIM\n[0] - NÂO\n\n>> '))  
 
-print('\nMuito obrigado por jogar!!')
+print('\n\t=-=-=-=-= Muito obrigado por jogar!! =-=-=-=-=')
